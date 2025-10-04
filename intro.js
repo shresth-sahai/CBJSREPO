2a0e4aebd5b3fbbad7e9f338e79cd23e3b2d542f
Shresth Sahai <shresth.sahai@shresths-ltmhdcg.internal.salesforce.com>
2025-10-04 19:47:55 +0530

    code

---
+++ intro.js
@@ -0,0 +1,73 @@
+// High level , interpreted , browser -> node js 
+// primary purpose is to create interactive web pages 
+// browser -> 
+// server -> node js 
+
+// dynamic typed , single thread async capabilities 
+
+// everyhting in  is an object 
+
+// functions are first class citizens 
+
+// type of variables 
+var a;  // function scoped can be redeclared 
+let b; // block scoped cannot be redeclared 
+const c=1; // block scoped cannot be reassigned 
+
+// function a(){
+
+// }
+
+// {
+//     let x=2; 
+// }
+// not possibble
+// let x=0;
+// let x="Hi"'
+
+// let x=10;
+// console.log(x)
+// {
+//     let x=2;
+//     console.log(x)
+// }
+// console.log(x)
+
+
+// hoisting  -> var functions 
+
+// console.log(z); // undefined 
+// var z;
+
+
+// console.log(x); //undefined 
+// {
+//     var x=1; 
+// }
+// console.log(x); // 1
+
+
+// console.log(a);
+// let a=1; // reference error 
+
+
+// console.log(s);  // undefined
+// var s=1;
+// console.log(s);  // 1 
+
+// console.log(s1);  // undefined
+// var s1;
+
+//  console.log(f); // [Function]
+
+
+// function f(){
+//     console.log("Hello"); 
+// }
+
+// f();
+
+
+
+
+
