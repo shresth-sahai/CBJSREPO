4c7eb10c2e5f55e38c289e1085dd645e9208c514
shresth-sahai <shresth.sahai@salesforce.com>
2025-11-15 19:35:46 +0530

    promise all

---
+++ axiosprops.js
@@ -0,0 +1,65 @@
+
+
+axios.interceptors.response(
+    // metadata -> config 
+    response =>{
+        console.log("Response",response.config.url);
+        console.log(response.status)
+        return response;
+    },
+    error =>{
+        console.log("Response",error.message);
+        console.log(response.status)
+    }
+)
+
+async function basicGet() {
+    
+    try{
+
+    }
+    catch{
+
+    }
+
+}
+
+async function getWithHeaders() {
+    try{
+        const res=await axios.get('',{
+            // metadata 
+            headers:{
+                'Accept':'application/json',
+            }
+        })
+    }
+    catch{
+
+    }
+}
+async function getWithQueryParam() {
+    try{
+        const res= await axios.get(
+            '',{
+                params:{
+                    limit:5,
+                    format:'json'
+                }
+            }
+        )
+    }
+}
+
+// promise -> 3 api end point ->axios -> promise 
+//concurrent requests 
+const res= await Promise.all(
+    [
+        axios.get(''),
+        axios.get('',{
+            params:{
+
+            }
+        })
+    ]
+)
+
