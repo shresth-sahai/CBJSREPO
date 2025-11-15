641782b884c508f7380eda92dfe6ca835f3e0d5a
shresth-sahai <shresth.sahai@salesforce.com>
2025-11-15 20:03:24 +0530

    window object prop

---
+++ bom.js
@@ -0,0 +1,23 @@
+
+let counter=0;
+let intervalId=null;
+
+
+function showSize(){
+    document.getElementsById('sizeInfo').innerHTMl=`
+        <strong> Window Size : </strong> <br>
+        ${window.innerWidth}
+        ${window.innerHeight}
+        ${window.outerHeight}
+        ${window.scrollX
+            
+        $(window.location.href)
+
+        window.location.reload
+        , 
+        window.open()
+
+        }
+    `
+}
+
