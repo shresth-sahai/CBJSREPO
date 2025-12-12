59f8ddf57e15f20e6382f28246deb98f99479738
shresth-sahai <shresth.sahai@salesforce.com>
2025-12-12 20:57:29 +0530

    httpmmethods

--- backend_http_method_api/server.js
+++ backend_http_method_api/server.js
@@ -22,7 +22,7 @@ app.get("tasks")
 // put method -> replace entire resource 
 //request body -> payload 
 app.put("/tasks/:id",(req,res)=>{
-    const index=tasks.find(t=>t.id == req.params.id);
+    const index=tasks.find(t=>t.id === req.params.id);
     if(index==-1) return res.status(404).json({error:"Not Found"});
     tasks[index]={id:Number(req.params.id),title:req.body.title,done:req.body.done};
     res.json(tasks[index]);
@@ -30,7 +30,7 @@ app.put("/tasks/:id",(req,res)=>{
 
 // patch -> update only partial 
 app.patch("/tasks/:id",(req,res)=>{
-    const task=tasks.find(t=>t.id == req.params.id);
+    const task=tasks.find(t=>t.id === req.params.id);
     if(!task) return res.status(404).json({error:"Not Found"});
     if(req.body.title!==undefined) task.title=req.body.title;
     res.json(task);
